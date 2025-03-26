ae4253c4e2bba4420403c2c26fffa83602ac4f28
XtraS001 <xushengchin@gmail.com>
2025-03-26 23:38:34 +0800

    upload to upload

--- src/main/java/com/tutorial/helloworld/HelloController.java
+++ src/main/java/com/tutorial/helloworld/HelloController.java
@@ -2,6 +2,10 @@ package com.tutorial.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class HelloController {
@@ -10,4 +14,16 @@ public class HelloController {
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/api/hello/{resourceId}")
+    public String getString(@PathVariable String resourceId) {
+        return "Here is the resourceId: " + resourceId;
+    }
+    
+}
+
+class Profile {
+
+    private String name;
+    private String email;
 }
